492851bba7e0db92d828dbdb6f9a8c1e4c1d88f7
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-19 13:33:37 +0100

    test: call api service from app

--- usher-client/App.tsx
+++ usher-client/App.tsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NativeBaseProvider } from 'native-base';
 
 import theme from './styles/theme';
 import Auth from './content/pages/Auth';
 import MainStack from './content/navigations/MainStack';
+import getCityEvents from './services/api/getCityEvents';
 
 export default function App() {
   const [user, setUser] = useState(null);
+
+  //! ONLY FOR DEBUGGING
+  useEffect(()=>getCityEvents('Barcelona').then(console.log), [])
 
   return (
     <NativeBaseProvider theme={theme}>
