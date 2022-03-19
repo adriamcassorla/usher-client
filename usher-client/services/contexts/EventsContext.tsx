c06e54a786c9b2e3f1b41e3bc065160989561a01
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-19 15:27:28 +0100

    feat: events context and types

---
+++ usher-client/services/contexts/EventsContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState } from 'react';
+import getCityEvents from '../api/getCityEvents';
+
+type EventsContextType = {
+  events: EventType[] | null;
+  populateEvents: (city: string) => void;
+}
+
+const defaultValue : EventsContextType = {
+  events: null,
+  populateEvents: () => {},
+};
+
+const initialEventsState: EventType[] = []
+
+export const EventsContext = createContext<EventsContextType>(defaultValue);
+
+export const EventsProvider = ({ children }: any) => {
+  const [events, setEvents] = useState(initialEventsState);
+
+  const populateEvents = async (city: string) => {
+    const fetchedEvents = await getCityEvents(city);
+    setEvents(fetchedEvents)
+  }
+
+  return (
+  <EventsContext.Provider value={{events, populateEvents}}>
+    {children}
+  </EventsContext.Provider>);
+};
+
+export const useEventsContext = () => useContext(EventsContext);
