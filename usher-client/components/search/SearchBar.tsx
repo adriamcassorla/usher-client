9888049c2ce5edfa64e298853b4a64c47c9d6657
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-22 12:00:47 +0100

    feat: final design and search feedback

--- usher-client/components/search/SearchBar.tsx
+++ usher-client/components/search/SearchBar.tsx
@@ -5,28 +5,36 @@ import * as React from 'react';
 type Props = {
   events: EventType[] | null;
   setResults: React.Dispatch<React.SetStateAction<EventType[] | null>>;
+  setIsSearching: React.Dispatch<React.SetStateAction<Boolean>>;
 };
 
-const SearchBar = ({ events, setResults }: Props) => {
+const SearchBar = ({ events, setResults, setIsSearching }: Props) => {
   const safeAreaProps = useSafeArea({
     safeAreaTop: true,
   });
 
   const handleChange = (input: string) => {
+    if (input === '') {
+      setIsSearching(false)
+      setResults([]);
+      return;
+    }
+
+    setIsSearching(true);
+
     const regex = new RegExp(`${input}`, 'i');
     const res = events?.filter(
-      (event) =>
-        regex.test(event.name) || regex.test(event.venue.name)
-    );
-    setResults(res || []);
+      (event) => regex.test(event.name) || regex.test(event.venue.name)
+      );
+      setResults(res || []);
   };
 
   return (
     <View pb={2} {...safeAreaProps}>
       <Input
+        mt={2}
         onChangeText={handleChange}
         alignSelf={'center'}
-        mt={1}
         placeholder="Search an event"
         placeholderTextColor="light.900"
         variant="filled"
--- usher-client/content/pages/Search.tsx
+++ usher-client/content/pages/Search.tsx
@@ -8,13 +8,12 @@ import SearchBar from '../../components/search/SearchBar';
 import { EventsContext } from '../../services/contexts/EventsContext';
 import { useFocusEffect } from '@react-navigation/native';
 import GradientProvider from '../../components/GradientProvider';
+import { BlurView } from 'expo-blur';
 
 const Search = () => {
-
   const { events } = useContext(EventsContext);
   const [results, setResults] = useState<EventType[] | null>(null);
-
-
+  const [isSearching, setIsSearching] = useState<Boolean>(false);
   type renderParams = {
     item: EventType;
   };
@@ -25,13 +24,28 @@ const Search = () => {
 
   return (
     <GradientProvider>
-      <View w="full" h="full" alignItems='center'>
+      <View w="full" h="full" alignItems="center">
         <SearchBar
           events={events}
           setResults={setResults}
+          setIsSearching={setIsSearching}
         />
+        {isSearching && (
+          <Text
+            w="90%"
+            textAlign="left"
+            fontSize="md"
+            fontWeight="medium"
+            color="white"
+            mb={2}
+          >
+            {results?.length
+              ? `Found ${results.length} event${results.length > 1 ? 's' : ''} matching your search`
+              : 'No results. Try something else!'}
+          </Text>
+        )}
         <FlatList
-          width='100%'
+          width="100%"
           data={results}
           renderItem={_renderItem}
           keyExtractor={(item) => String(item.id)}
