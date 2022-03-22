bf0af783984294c438df8ba2a759f4e37a72179d
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-22 18:27:18 +0100

    chore: break out filtering logic

--- usher-client/components/home/HomeList.tsx
+++ usher-client/components/home/HomeList.tsx
@@ -2,11 +2,12 @@ import { Button, Divider, FlatList, Text } from 'native-base';
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { filterEvents } from '../../utils/helpers/filter';
 import { Highlights, FilterMenu, EventCard } from './';
 
 type Props = { events: EventType[] | null };
 type renderParams = {
-  item: EventType | "top" | "filter" | "divider";
+  item: EventType | 'top' | 'filter' | 'divider';
 };
 
 const HomeList = ({ events }: Props) => {
@@ -16,31 +17,11 @@ const HomeList = ({ events }: Props) => {
   const [filtered, setFiltered] = useState(events);
   const [filters, setFilters] = useState<string[]>([]);
 
-  useEffect(() => setFiltered(filterEvents()), [filters]);
-
-  const filterEvents = () => {
+  useEffect(() => {
     if (events) {
-      let evs = events;
-      if (filters.includes('Today'))
-        evs = evs.filter((e) => e.today_shows.length);
-      if (filters.includes('Under 20€'))
-        evs = evs?.filter((e) => e.price <= 20);
-
-      let genreFilters: string[] = [];
-      if (filters.includes('Comedy'))
-        genreFilters.push('Comedia', 'Comèdia', 'Tragicomedia');
-      if (filters.includes('Drama')) genreFilters.push('Tragedia', 'Tragicomedia');
-      if (filters.includes('Musical')) genreFilters.push('Musical');
-
-      if (genreFilters.length) {
-        evs = evs.filter((ev) =>
-          genreFilters.some((genre) => ev.genres.includes(genre))
-        );
-        return evs;
-      } else return evs;
+      setFiltered(filterEvents(events, filters));
     }
-    return [];
-  };
+  }, [filters]);
 
   const _renderItem = ({ item }: renderParams) => {
     if (item === 'top') return <Highlights />;
@@ -53,15 +34,6 @@ const HomeList = ({ events }: Props) => {
           isOnTop={isOnTop}
         />
       );
-    //TODO: Add clear filters button and number hidden feedback
-    // if (item === 'reset_button')
-    //   return (
-    //     <Button variant="outline" style={{borderColor: 'white', borderRadius: 20, width:'70%'}}onPress={() => dispatch('init')}>
-    //       <Text color='white'>
-    //         Clear filters
-    //         </Text>
-    //     </Button>
-    //   );
     return <EventCard event={item} />;
   };
   if (!events) return <Text>Loading...</Text>;
@@ -69,13 +41,13 @@ const HomeList = ({ events }: Props) => {
   return (
     <FlatList
       // @ts-ignore
-      data={['top', 'divider', 'filter', ...filtered /*, 'reset_button'*/]}
+      data={['top', 'divider', 'filter', ...filtered]}
       stickyHeaderIndices={[2]}
       removeClippedSubviews={true}
       initialNumToRender={5}
       renderItem={_renderItem}
       keyExtractor={(item) =>
-        typeof item === "string" ? item : String(item.id)
+        typeof item === 'string' ? item : String(item.id)
       }
       onScroll={(e) => {
         const yPos = e.nativeEvent.contentOffset.y;
