87c5fbb6dbb4a151923ccf4ed160fbfae9d9178c
Adrià M. Cassorla <adria@adriamartinez.cat>
2022-03-22 19:17:17 +0100

    fix(user): added user to context

--- usher-client/App.tsx
+++ usher-client/App.tsx
@@ -1,33 +1,18 @@
 import * as React from "react";
-import { useState } from "react";
 import { NativeBaseProvider } from "native-base";
 
-import { UserContext, UserProvider } from "./services/contexts/UserContext";
+import { UserProvider } from "./services/contexts/UserContext";
 
 import theme from "./styles/theme";
-import Auth from "./content/pages/Auth";
-import MainStack from "./content/navigation/MainStack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { getLastUser } from "./utils/helpers/login";
-import { convertAbsoluteToRem } from "native-base/lib/typescript/theme/tools";
+import Content from "./content";
 
 export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-  const [status, setStatus] = useState<"loading" | "loaded">("loading");
-
-  React.useEffect(() => {
-    getLastUser().then((user) => {
-      setUser(user);
-      setStatus("loaded");
-    });
-  }, []);
-
-  if (status === "loading") return null;
   return (
     <UserProvider>
       <SafeAreaProvider>
         <NativeBaseProvider theme={theme}>
-          {user ? <MainStack></MainStack> : <Auth setUser={setUser}></Auth>}
+          <Content></Content>
         </NativeBaseProvider>
       </SafeAreaProvider>
     </UserProvider>
