93cc13deef3a95139ba4dd87e48b62ac603442c2
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-22 15:57:45 +0100

    style: filter list

--- usher-client/components/home/HomeList.tsx
+++ usher-client/components/home/HomeList.tsx
@@ -1,39 +1,65 @@
-import { FlatList, Text } from "native-base";
-import * as React from "react";
-import { useState } from "react";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Highlights, FilterMenu, EventCard } from "./";
+import { Button, Divider, FlatList, Text } from 'native-base';
+import * as React from 'react';
+import { useReducer, useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Highlights, FilterMenu, EventCard } from './';
 
 type Props = { events: EventType[] | null };
 type renderParams = {
-  item: EventType | "top" | "filter";
+  item: EventType | 'top' | 'filter' | 'divider' | 'reset_button';
 };
 
 const HomeList = ({ events }: Props) => {
-  if (!events) return <Text>Loading...</Text>;
-
-  const [isOnTop, setIsOnTop] = useState(false);
   const { top } = useSafeAreaInsets();
+  const [isOnTop, setIsOnTop] = useState(false);
+
+  const [state, dispatch] = useReducer(reducer, events);
+
+  function reducer(state: any, action: any) {
+    switch (action) {
+      case 'Comedy':
+        console.log('Comedy button hit');
+        return state.filter((e) => e.genres.includes('Comedia'));
+      case 'init':
+        console.log('clearing filters');
+        return events;
+      default:
+        console.log('reducer activated with action', action);
+        return state;
+    }
+  }
 
   const _renderItem = ({ item }: renderParams) => {
-    if (item === "top") return <Highlights />;
-    if (item === "filter") return <FilterMenu isOnTop={isOnTop} />;
+    if (item === 'top') return <Highlights />;
+    if (item === 'divider') return <Divider m="auto" w="85%" />;
+    if (item === 'filter')
+      return <FilterMenu dispatch={dispatch} isOnTop={isOnTop} />;
+    // if (item === 'reset_button')
+    //   return (
+    //     <Button variant="outline" style={{borderColor: 'white', borderRadius: 20, width:'70%'}}onPress={() => dispatch('init')}>
+    //       <Text color='white'>
+    //         Clear filters
+    //         </Text>
+    //     </Button>
+    //   );
     return <EventCard event={item} />;
   };
+  if (!events) return <Text>Loading...</Text>;
 
   return (
     <FlatList
-      data={["top", "filter", ...events]}
-      stickyHeaderIndices={[1]}
+      // @ts-ignore
+      data={['top', 'divider', 'filter', ...state/*, 'reset_button'*/]}
+      stickyHeaderIndices={[2]}
       removeClippedSubviews={true}
       initialNumToRender={5}
       renderItem={_renderItem}
       keyExtractor={(item) =>
-        typeof item === "string" ? item : String(item.id)
+        typeof item === 'string' ? item : String(item.id)
       }
       onScroll={(e) => {
         const yPos = e.nativeEvent.contentOffset.y;
-        setIsOnTop(yPos >= 370 - top);
+        setIsOnTop(yPos >= 380 - top);
       }}
     />
   );
