daadff5e4761bc90f9725b045795693888a1dc06
Nico Lopez <nicolopez413@gmail.com>
2022-03-24 16:11:39 +0100

    Changed apiURL to tourn

--- usher-client/services/api/tickets.ts
+++ usher-client/services/api/tickets.ts
@@ -1,8 +1,8 @@
 import { gql, GraphQLClient } from "graphql-request";
 import { AsyncStorage } from "react-native";
 
-const apiURL = "http://localhost:4004";
-// const apiURL = "https://tourn.me/usher";
+// const apiURL = "http://localhost:4004";
+const apiURL = "https://tourn.me/usher";
 
 const client = new GraphQLClient(apiURL);
 
