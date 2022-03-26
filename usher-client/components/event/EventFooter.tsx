0eefe6690ace73facec771c5137254e9ad2d8b2e
Adrian Suarez del Busto <adrian.s.delbusto@gmail.com>
2022-03-26 16:39:34 +0100

    fix: keyboard avoid view in payment

--- usher-client/components/event/EventFooter.tsx
+++ usher-client/components/event/EventFooter.tsx
@@ -1,4 +1,4 @@
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native';
 import {
   Button,
   Center,
@@ -8,20 +8,29 @@ import {
   VStack,
   Actionsheet,
   useDisclose,
-} from "native-base";
-import * as React from "react";
-import { useState, useEffect } from "react";
-import { Dimensions } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { MainStackNavType } from "../../utils/Types/navTypes";
-import EventPayment from "./EventPayment";
-import TicketSelector from "./TicketSelector";
+} from 'native-base';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Dimensions } from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { MainStackNavType } from '../../utils/Types/navTypes';
+import EventPayment from './EventPayment';
+import TicketSelector from './TicketSelector';
 
-const EventFooter = ({ price, shows, eventName }: { price: number; shows: Show[]; eventName: string; }) => {
+const EventFooter = ({
+  price,
+  shows,
+  eventName,
+}: {
+  price: number;
+  shows: Show[];
+  eventName: string;
+}) => {
   let { bottom } = useSafeAreaInsets();
   bottom = bottom > 22 ? bottom : 22;
   const height = 60 + bottom;
-  const top = Dimensions.get("screen").height - height;
+  const top = Dimensions.get('screen').height - height;
   const { isOpen, onOpen, onClose } = useDisclose();
   const navigation = useNavigation<MainStackNavType>();
   const [seats, setSeats] = useState(0);
@@ -39,59 +48,60 @@ const EventFooter = ({ price, shows, eventName }: { price: number; shows: Show[]
 
   return (
     <Center
-      position={"absolute"}
+      position={'absolute'}
       top={top}
-      w={"full"}
+      w={'full'}
       h={height}
-      bg={"light.50"}
+      bg={'light.50'}
       pb={`${bottom}px`}
       shadow={5}
     >
       <Flex
-        flexDirection={"row"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
+        flexDirection={'row'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
         px={10}
         pt={3}
-        w={"full"}
+        w={'full'}
       >
-        <VStack alignItems={"center"}>
-          <Heading size={"xl"} color={"dark.200"}>
+        <VStack alignItems={'center'}>
+          <Heading size={'xl'} color={'dark.200'}>
             {price}€
           </Heading>
-          <Text color={"primary.400"}>{seats} seats left!</Text>
+          <Text color={'primary.400'}>{seats} seats left!</Text>
         </VStack>
-        <Button rounded={40} shadow={5} w={"60%"} onPress={onOpen}>
-          <Text color={"light.50"} fontSize={"lg"} fontWeight={"700"}>
+        <Button rounded={40} shadow={5} w={'60%'} onPress={onOpen}>
+          <Text color={'light.50'} fontSize={'lg'} fontWeight={'700'}>
             Take your seats
           </Text>
         </Button>
         <Actionsheet isOpen={isOpen} onClose={onClose}>
           <Actionsheet.Content>
-            { ticketSelected ?
+            {ticketSelected ? (
               <EventPayment
-              setTicketSelected={setTicketSelected}
-              showId={showId}
-              onClose={onClose}
-              navigation={navigation}
-              quantity={quantity}
-              eventName={eventName}
-              price={price}
+                setTicketSelected={setTicketSelected}
+                showId={showId}
+                onClose={onClose}
+                navigation={navigation}
+                quantity={quantity}
+                eventName={eventName}
+                price={price}
               ></EventPayment>
-            :
+            ) : (
               <TicketSelector
-              shows={shows}
-              seats={seats}
-              navigation={navigation}
-              onClose={onClose}
-              setTicketSelected={setTicketSelected}
-              setShowId={setShowId}
-              showId={showId}
-              quantity={quantity}
-              setQuantity={setQuantity}
+                shows={shows}
+                seats={seats}
+                navigation={navigation}
+                onClose={onClose}
+                setTicketSelected={setTicketSelected}
+                setShowId={setShowId}
+                showId={showId}
+                quantity={quantity}
+                setQuantity={setQuantity}
               ></TicketSelector>
-            }
+            )}
           </Actionsheet.Content>
+          <KeyboardSpacer />
         </Actionsheet>
       </Flex>
     </Center>
